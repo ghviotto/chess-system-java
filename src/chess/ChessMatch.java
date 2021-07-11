977fd4aa4e6b9f6b0e554b251af343ba9f848bd6
Gustavo Haidar Viotto <ghviotto@gmail.com>
2021-07-11 15:51:11 -0300

    Possiveis movimentos

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -41,7 +41,10 @@ public class ChessMatch {
 	
 	private void validateSourcePosition(Position position) {
 		if (!board.thereIsAPiece(position)) {
-			throw new ChessException("Não existe peça na posição de Origem!");
+			throw new ChessException("Nao existe peca na posicao de origem!");
+		}
+		if (!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("Nao existem movimentos possiveis para a peca selecionada!");
 		}
 	}
 	
